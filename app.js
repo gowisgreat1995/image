2d29068cdeec042c8a715e86181e499b6821372c
gowisgreat1995 <gowisgreat1995@gmail.com>
2017-07-30 11:12:38 +0530

    final

--- app.js
+++ app.js
@@ -25,6 +25,60 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
+app.get('/api/image/:search',function(req,res){
+    var now=new Date();
+  var search=req.params.search;
+    if(req.query.offset)
+    var offset=req.query.offset;
+    else
+ var offset=1;
+var now_utc = now.getUTCFullYear()+"-"+(now.getUTCMonth()+1)+"-"+now.getUTCDate()+"T"+now.getUTCHours()+":"+now.getUTCMinutes()+":"+now.getUTCSeconds()+":"+now.getUTCMilliseconds();
+    
+    const GoogleImages = require('google-images');
+ 
+const client = new GoogleImages(process.env.cx, process.env.key);
+
+
+    var mongo = require('mongodb').MongoClient;
+    mongo.connect('mongodb://gowisgreat1995:'+process.env.db_password+'@cluster0-shard-00-00-1hsjf.mongodb.net:27017,cluster0-shard-00-01-1hsjf.mongodb.net:27017,cluster0-shard-00-02-1hsjf.mongodb.net:27017/'+process.env.db_name+'?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin',
+                  
+                  function(err, db) {
+
+       
+        var image=db.collection("imageSearches");
+        
+       image.insert({
+           "index":req.params.search,"time":now_utc
+       },function(err,docs){
+           if(docs)
+               client.search(search,{page:offset}).then(images=>{res.send(images);res.end();});
+           
+       });
+  
+        } );
+});
+
+app.get("/api/latest/",function(req,res){
+    
+    
+      var mongo = require('mongodb').MongoClient;
+    mongo.connect('mongodb://gowisgreat1995:'+process.env.db_password+'@cluster0-shard-00-00-1hsjf.mongodb.net:27017,cluster0-shard-00-01-1hsjf.mongodb.net:27017,cluster0-shard-00-02-1hsjf.mongodb.net:27017/'+process.env.db_name+'?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin',
+                  
+                  function(err, db) {
+
+       
+        var image=db.collection("imageSearches");
+        var dataArray=[];
+      image.find({},{
+          "index":1,
+          "time":1
+      }).toArray(function(err,docs){
+        res.send(docs);
+      res.end();});
+        } );
+     
+    
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -43,4 +97,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.listen(8000);
 module.exports = app;
